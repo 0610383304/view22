281f4a1285f02256de9b312f565d86d774cd8ab8
Your Name <benbew2122@gmail.com>
2018-06-16 12:43:03 +0700

    program

---
+++ index.vue
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Vue.js - Hello</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.js"></script>
+</head>
+<body>
+
+    <div id="vue-app">
+        <h1>{{ histore}} </h1>
+        <h3>{{ name }} </h3>
+        <h3>{{ nickname }} </h3>
+        <h3>{{ age }}  </h3>
+        <h3>{{ birdth }} </h3>
+        <h3>{{ addess }} </h3>
+    </div>
+    
+    <script src="app.js" charset="utf-8"></script>
+</body>
+</html>
